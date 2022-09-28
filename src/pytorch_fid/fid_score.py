63a1d88e212cf92362a580bed4e77d340fba2570
Kevin Xie <kevin.cheng.xie@gmail.com>
2022-09-28 19:08:27 -0400

    Added some hack functions to expose internal API

--- src/pytorch_fid/fid_score.py
+++ src/pytorch_fid/fid_score.py
@@ -261,6 +261,68 @@ def calculate_fid_given_paths(paths, batch_size, device, dims, num_workers=1):
 
     return fid_value
 
+def hack_get_activations(batch, model, device):
+    batch = batch.to(device)
+
+    with torch.no_grad():
+        pred = model(batch)[0]
+
+    # If model output is not scalar, apply global spatial average pooling.
+    # This happens if you choose a dimensionality not equal 2048.
+    if pred.size(2) != 1 or pred.size(3) != 1:
+        pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+    pred = pred.squeeze(3).squeeze(2).cpu().numpy()
+    return pred
+
+def hack_comp_stats_from_activations(act):
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
+
+def hack_get_stats_from_dataset(dataset, 
+        model, batch_size=50, dims=2048, device='cpu', num_workers=1):
+    """
+    Hack of get_activations that accepts my dataset format
+    """
+    model.eval()
+    dataloader = torch.utils.data.DataLoader(dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             drop_last=False,
+                                             num_workers=num_workers)
+    # pred_arr = np.empty((len(files), dims))
+    pred_arr = np.empty((len(dataset), dims))
+    start_idx = 0
+
+    for batch in tqdm(dataloader):
+        # strip out dummy label placeholder
+        batch = batch[0]
+        pred = hack_get_activations(batch, model, device)
+        pred_arr[start_idx:start_idx + pred.shape[0]] = pred
+        start_idx = start_idx + pred.shape[0]
+
+    act = pred_arr
+    mu, sigma = hack_comp_stats_from_activations(act)
+    return mu, sigma
+    # return pred_ar
+
+def hack_get_fid_model(device, dims=2048):
+    block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
+    model = InceptionV3([block_idx]).to(device)
+    return model
+
+def hack_calculate_fid_from_dataset(dataset,device,dims=2048):
+    """Calculates the FID of dataset"""
+    block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
+    model = InceptionV3([block_idx]).to(device)
+
+    act = get_activations(files, model, batch_size, dims, device, num_workers)
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
+
 
 def main():
     args = parser.parse_args()
